3516d2ee55909c8425105a22e4795fac97cb2857
sdubois18 <samdubois18@icloud.com>
2020-07-06 23:07:33 -0400

    Added database model and code to initialize the database

---
+++ website-server/__init__.py
@@ -0,0 +1,40 @@
+#
+#  __init__.py
+#  Website Server
+#
+#  Created by Sam DuBois on 6/25/20.
+#
+
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when NOT testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test test_config
+        app.config.from_mapping(test_config)
+
+    # ensure that the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World'
+
+    from . import db
+    db.init_app(app)
+
+    return app
